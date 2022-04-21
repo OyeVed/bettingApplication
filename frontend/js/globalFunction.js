d8c6d0a0d80a4cddfcbf88771a9fdf9659241b97
sahoochinmay <sahoochinmay54@gmail.com>
2022-04-21 15:48:13 +0530

    Alert integration completed

---
+++ frontend/js/globalFunction.js
@@ -0,0 +1,15 @@
+// show alert
+function showAlert(type, msg) {
+  $(".myAlert-top").hide();
+  let alertComponent = document.getElementById("alert_div");
+  if (type === "success") {
+    alertComponent.className = "myAlert-top alert alert-success";
+  } else if (type === "danger") {
+    alertComponent.className = "myAlert-top alert alert-danger";
+  }
+  alertComponent.innerHTML = `<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>${msg}`;
+  $(".myAlert-top").show();
+  setTimeout(function () {
+    $(".myAlert-top").hide();
+  }, 3000);
+}
