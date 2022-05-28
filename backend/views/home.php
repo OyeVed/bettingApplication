1bd452aa0d3d5f1f8b55db84972ff2e2925a6a67
krishnpal-oyesters <krishnpal@oyesters.in>
2022-05-28 16:12:36 +0530

    updated status in home route

--- backend/views/home.php
+++ backend/views/home.php
@@ -15,7 +15,11 @@ if(auth($token)){
     if($query->execute() && $query->rowCount() != 0){
         $markets = $query->fetchAll(PDO::FETCH_OBJ);
         foreach ($markets as $market) {
-            $status = "ok";
+            if(date($market->market_closetime) > date("H-i-s")){
+                $status = "open";
+            }else{
+                $status = "close";
+            }
             $details = [
                 "market_fullname" => $market->market_fullname,
                 "market_opentime" => $market->market_opentime,
@@ -36,74 +40,3 @@ if(auth($token)){
         ];
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $game_list = [
-//     [
-//         "game" => "Main Baazar",
-//         "game_status" => "Open",
-//         "open_time" => "9:40pm",
-//         "close_time" => "11:59pm",
-//         "result" => "xxx-xx-xxx"
-//     ],
-//     [
-//         "game" => "Milan Day",
-//         "game_status" => "Close",
-//         "open_time" => "9:40pm",
-//         "close_time" => "11:59pm",
-//         "result" => "xxx-xx-xxx"
-//     ],
-//     [
-//         "game" => "Milan Night",
-//         "game_status" => "Cpen",
-//         "open_time" => "9:40pm",
-//         "close_time" => "11:59pm",
-//         "result" => "xxx-xx-xxx"
-//     ],
-//     [
-//         "game" => "Time Baazar",
-//         "game_status" => "Open",
-//         "open_time" => "9:40pm",
-//         "close_time" => "11:59pm",
-//         "result" => "xxx-xx-xxx"
-//     ],
-// ];
-
-
-// $status = 200;
-
-// $response = [
-//     "game_list" => $game_list
-// ];
