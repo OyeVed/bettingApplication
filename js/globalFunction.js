e51d8e0952d83b1ed0e055fc86e26f66ebdfd011
sahoochinmay <sahoochinmay54@gmail.com>
2022-05-29 18:16:22 +0530

    sign out completed

--- js/globalFunction.js
+++ js/globalFunction.js
@@ -1,4 +1,3 @@
-
 function startLoader() {
   $("div.spanner").addClass("show");
   $("div.overlay").addClass("show");
@@ -25,6 +24,76 @@ function navigate(path) {
 
 const axiosInstance = axios.create({
   baseURL: `/bettingApplication/backend/`,
-  credentials: 'include',
+  credentials: "include",
   withCredentials: true,
-})
+});
+
+function parseJwt(token) {
+  var base64Url = token.split(".")[1];
+  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  var jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+
+  return JSON.parse(jsonPayload);
+}
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  let cookieNow = "";
+  let cookieData = null;
+  if (parts.length === 2) cookieNow = parts.pop().split(";").shift();
+  if (cookieNow !== "") {
+    cookieData = parseJwt(cookieNow);
+    return cookieData;
+  }
+  return cookieData;
+}
+
+function signOut() {
+  startLoader();
+  let cookieData = getCookie("jwt");
+  axiosInstance
+    .post("logout", {
+      phone_number: cookieData?.userName,
+    })
+    .then(
+      (response) => {
+        endLoader();
+        if (response.status === 200) {
+          location.href = "index.html";
+        } else {
+          $.notify(
+            {
+              title: "",
+              message: response.data?.msg,
+              icon: "fa fa-times",
+            },
+            {
+              type: "danger",
+            }
+          );
+        }
+      },
+      (error) => {
+        endLoader();
+        console.log("error", error);
+        $.notify(
+          {
+            title: "",
+            message: `Something went wrong! Please try again.`,
+            icon: "fa fa-times",
+          },
+          {
+            type: "danger",
+          }
+        );
+      }
+    );
+}
