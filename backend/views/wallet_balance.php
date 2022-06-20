0f3f792360dfbb49ce9575304fe17b1b9271ab6f
krishnpal-oyesters <krishnpal@oyesters.in>
2022-06-20 20:51:03 +0530

    wallet_balance route updated

--- backend/views/wallet_balance.php
+++ backend/views/wallet_balance.php
@@ -20,47 +20,60 @@ $token = $_COOKIE["jwt"];
 
 // checking is the user authorized 
 if(auth($token)){
-    $available_amount = 0;
     //Total deposit on a particular day
     // $secret_key = "bGS6lzFqvvSQ8ALbOxatm7/Vk7mLQyzqaS34Q4oR1ew=";
     $payload = JWT::decode($token, new Key($SECRET_KEY, 'HS512'));
-    $available_amount = 0;
+    
+    try{
+        $wallet = (object) [];
 
-    //calculating funds available
-    $query = $con->prepare("
-    SELECT
-    amount_in_wallet
-    FROM transaction_details
-    WHERE user_id=:user_id
-    ORDER BY transaction_id DESC
-    LIMIT 1 ");
-    $query->bindParam(':user_id', $payload->user_id, PDO::PARAM_STR);
-    if($query->execute()){
-        $query->setFetchMode(PDO::FETCH_ASSOC);
-        while($row = $query->fetch()) {
-            $available_amount = $row['amount_in_wallet'];
+        //calculating funds available
+        $query = $con->prepare("
+        SELECT
+        amount_in_wallet
+        FROM transaction_details
+        WHERE user_id=:user_id
+        ORDER BY transaction_id DESC
+        LIMIT 1 ");
+        $query->bindParam(':user_id', $payload->user_id, PDO::PARAM_STR);
+        if($query->execute()){
+            $query->setFetchMode(PDO::FETCH_ASSOC);
+            while($row = $query->fetch()) {
+                $available_amount = $row['amount_in_wallet'];
+            }
+            $wallet->available_amount = $available_amount;
+        }else{
+            $status = 203;
+            $response = [
+                "msg" => "Wallet can't be fetched for now."
+            ];
         }
-    }else{
-        $status = 203;
-        $response = [
-            "msg" => "Wallet can't be fetched for now."
-        ];
-    }
 
-    $sql = "SELECT transaction_type, transaction_name, transaction_amount, created_at FROM transaction_details ORDER BY transaction_id desc
-    WHERE user_id=:user_id";
-    $query = $con -> prepare($sql);
-    $query->bindParam(':user_id', $payload->user_id, PDO::PARAM_STR);
-    if($query->execute()){
-        $statement = $query->fetchAll(PDO::FETCH_OBJ);
+        $sql = "SELECT transaction_type, transaction_name, transaction_amount, created_at FROM transaction_details 
+        WHERE user_id=:user_id ORDER BY transaction_id desc ";
+        $query = $con -> prepare($sql);
+        $query->bindParam(':user_id', $payload->user_id, PDO::PARAM_STR);
+        if($query->execute()){
+            $statement = $query->fetchAll(PDO::FETCH_OBJ);
+            $wallet->statement = $statement;
+        }else{
+            $status = 203;
+            $response = [
+                "msg" => "Can't fetch users statement."
+            ];
+        }
+
+        //success response
         $status = 200;
         $response = [
-            "msg" => $statement
+            "msg" => "Wallet data fetched successfully",
+            "wallet" => $wallet
         ];
-    }else{
+    }catch(Exception $e){
         $status = 203;
         $response = [
-            "msg" => "Can't fetch users statement."
+            "msg" => "Wallet data can't be fetched.",
+            "error" => $e->getMessage()
         ];
     }
 
