8036d9639ad708ecffa683c9f596adc195fa5cc1
krishnpal-oyesters <krishnpal@oyesters.in>
2022-06-25 00:39:07 +0530

    market days parameter added

--- backend/views/home.php
+++ backend/views/home.php
@@ -9,13 +9,19 @@ $token = $_COOKIE["user_jwt"];
 // checking is the user authorized 
 if(auth($token)){
     $game_list = [];
+    $curr_day = date('l');
     $sql = "SELECT * FROM market_table";
     $query = $con -> prepare($sql);
     $query->execute();
     if($query->execute() && $query->rowCount() != 0){
         $markets = $query->fetchAll(PDO::FETCH_OBJ);
         foreach ($markets as $market) {
-            if(date($market->market_closetime) > date("H-i-s")){
+            $market_on_days = $market->market_on_days;
+            $market_on_days = explode(",", $market_on_days);
+            echo"<pre>";
+            print_r($market_on_days);
+            echo "\n";
+            if((date($market->market_closetime) > date("H-i-s")) && (in_array($curr_day, $market_on_days))){
                 $status = "open";
             }else{
                 $status = "close";
